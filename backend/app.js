6bacc93fed409ae1a15ff30ef86c92420492354c
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-05-14 02:38:23 +0400

    added/cors

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
-const cors = require('./middlewares/cors');
 const limiter = require('./utils/limiter');
 const checkedErrors = require('./middlewares/error');
 const router = require('./routes/index');
@@ -14,6 +14,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+const corsOptions = {
+  origin: ['http://localhost:3000',
+    'https://mesto.blyaderuiner.nomoredomains.monster',
+    'https://api.mesto.blyaderuiner.nomoredomains.monster',
+  ],
+};
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -21,14 +27,14 @@ app.use(express.json());
 app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
-app.use(cors);
+app.use(cors(corsOptions));
 
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
-}); 
+});
 app.use(router);
 app.use(errorLogger);
 
