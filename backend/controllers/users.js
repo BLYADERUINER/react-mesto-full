8e59ccf45ee3fe2b23453d15954f07fe6dc048f5
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-05-14 22:55:38 +0400

    fixed/cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -85,7 +85,7 @@ const login = (req, res, next) => {
       const token = generateToken({ _id: user._id });
 
       res
-        .cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true })
+        .cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
         .send({ message: 'Авторизация прошла успешно' });
     })
     .catch(next);
