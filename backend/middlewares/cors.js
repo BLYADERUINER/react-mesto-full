d1f9ee07c9d0e1118a65a657a6c90f3b75aea8e1
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-05-15 13:41:01 +0400

    added/opencors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,21 +1,23 @@
 const allowedCors = [
-  'localhost:3001',
   'localhost:3000',
   'http://mesto.blyaderuiner.nomoredomains.monster',
   'https://mesto.blyaderuiner.nomoredomains.monster',
+  'http://api.mesto.blyaderuiner.nomoredomains.monster',
+  'https://api.mesto.blyaderuiner.nomoredomains.monster',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const cors = (req, res, next) => {
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+  res.header('Access-Control-Allow-Origin', "*");
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  //   res.header('Access-Control-Allow-Credentials', true);
+  // }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
